08e1a3f4c3dfc786d5cc4862e2cbdcc468127073
MrPanther0 <109415681+MrPanther0@users.noreply.github.com>
2024-02-10 14:59:13 +0530

    Create ip_changer.sh

---
+++ ip_changer.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+user="Mr.Panther"
+# Get the current IP address and subnet mask
+current_ip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]\.){3}[0-9]' | grep -Eo '([0-9]\.){3}[0-9]' | head -n 1)
+current_subnet=$(ifconfig | grep -Eo 'netmask ([0-9]\.){3}[0-9]' | grep -Eo '([0-9]\.){3}[0-9]' | head -n 1)
+
+# Generate a random IP address and subnet mask in the same supernet
+random_ip=$(echo $current_ip | cut -d '.' -f 1-2).$(shuf -i 1-254 -n 1).$(shuf -i 1-254 -n 1)
+random_subnet=$(echo $current_subnet | cut -d '.' -f 1-3).$(shuf -i 1-254 -n 1)
+
+# Change the IP address and subnet mask
+sudo ifconfig eth0 $random_ip netmask $random_subnet
+
+# Print the new IP address and subnet mask
+echo "This script is made by $user"
+echo "New IP address: $random_ip"
+echo "New subnet mask: $random_subnet"
+
+
+# Sleep for 2 minutes
+sleep 1
+
+# Repeat the process
+./ip_changer.sh
